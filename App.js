3831c565e54fc6ec98df0d891583b01eafadcec4
urvisailor <urvisailor@gmail.com>
2023-06-10 11:23:13 +0530

    project setup

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+function App() {
+
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+export default App;
